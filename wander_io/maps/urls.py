91447b3b01de2ced556101da092cc33e5e0852b2
thegajan <thegajannagaraj@gmail.com>
2020-02-20 23:19:22 -0800

    finished landing get

--- wander_io/maps/urls.py
+++ wander_io/maps/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='site-home'),
+    path('survey/', views.survey, name='site-survey'),
     path('about/', views.about, name='site-about'),
 ]
--- wander_io/maps/views.py
+++ wander_io/maps/views.py
@@ -5,5 +5,9 @@ def home(request):
     return render(request, 'maps/home.html')
 
 
+def survey(request):
+    return render(request, 'maps/survey.html')
+
+
 def about(request):
     return render(request, 'maps/about.html', {'title': 'About'})
